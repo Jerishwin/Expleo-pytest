717bd9880bc7f6cdf5802edc5831fd5ab001ce8f
Jerishwin Joseph <jerishwinj@gmail.com>
2026-06-09 16:19:36 +0530

    9-6-26

--- dataDriven/Test/test_dataDriven.py
+++ dataDriven/Test/test_dataDriven.py
@@ -3,29 +3,38 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from Utilities import excelReader
+from Utilities import logCreator
 
 @pytest.mark.parametrize("username,password",excelReader.get_data("ExcelFiles/data.xlsx","Sheet1"))
 class TestLogin:
+    logger = logCreator.log_generator()
     def test_log(self,username,password):
+        self.logger.info("Test started")
         self.driver = webdriver.Chrome()
         self.driver.maximize_window()
         self.driver.get("https://demoblaze.com/index.html")
+        self.logger.info("Site Launched")
 
         login_button = self.driver.find_element(By.ID,"login2")
         login_button.click()
+        self.logger.info("Login Button Clicked")
         self.driver.implicitly_wait(3)
 
         user_name = self.driver.find_element(By.ID,"loginusername")
         passord = self.driver.find_element(By.ID,"loginpassword")
 
         user_name.send_keys(username)
+        self.logger.info("Username filled")
         passord.send_keys(password)
+        self.logger.info("Passord Filled")
 
         self.driver.find_element(By.XPATH,"//button[text()='Log in']").click()
+        self.logger.info("Submit button clicked")
         time.sleep(5)
 
         user = self.driver.find_element(By.ID,"nameofuser")
         assert user.text.__eq__("Welcome "+username)
+        self.logger.info("Asserted in Dashboard")
 
         self.driver.quit()
 
