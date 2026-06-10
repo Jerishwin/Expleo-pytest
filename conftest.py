42a690a6e7f34866950e9004fa370758eb71a11a
Jerishwin Joseph <jerishwinj@gmail.com>
2026-06-10 14:44:21 +0530

    10-6-26

---
+++ conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from  read_config import get_config
+from selenium import webdriver
+
+@pytest.fixture(params=[get_config("browser","name")])
+def setup_teardown(request):
+    if request.param == "chrome":
+        driver = webdriver.Chrome()
+    elif request.param == "firefox":
+        driver = webdriver.Firefox()
+    elif request.param == "edge":
+        driver = webdriver.Edge()
+    driver.maximize_window()
+    driver.implicitly_wait(5)
+    driver.get(get_config("url","base"))
+    request.cls.driver=driver
+    yield
+    driver.quit()
